69b0187a5e7215e520fdf5e39d65f733eb48937c
YejunMoon <myj6873@gmail.com>
2025-11-10 17:16:01 +0900

    [Silver III] Title: 주유소, Time: 104 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -6,13 +6,12 @@ n = int(input())
 length = list(map(int, input().split()))
 price = list(map(int, input().split()))
 
-min_price = price[:n-2]
-min_price = min(min_price)
-for i in range(n):
-    if min_price == price[i]:             #첫번째 도시의 기름값이 제일 싸다면,
-        oil += min_price * sum(length[i:])
-        break
-    else:                                  # 아니라면, 다음 도시까지만 기름 값 넣고, 다음 도시에서 또 확인
-        oil += price[i] * length[i]
+minPrice=price[0]
+
+for i in range(n-1):
+    if minPrice > price[i]:
+        minPrice = price[i]
         
+    oil += minPrice*length[i]
+
 print(oil)
