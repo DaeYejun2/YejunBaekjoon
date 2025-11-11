50d2e958e32d420fafd0b4ff367bd9aa050392da
YejunMoon <myj6873@gmail.com>
2025-11-11 14:51:23 +0900

    [Silver III] Title: 파도반 수열, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,23 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-def dp(n):
-    if n >= 1:
-        d[1] = 1
-    if n >= 2:
-        d[2] = 1
-    if n >= 3:
-        d[3] = 1
-    if n >= 4:
-        d[4] = 2
-    if n >= 5:
-        d[5] = 2
-    for i in range(5, n+1):
-        d[i] = d[i-1] + d[i-5]
-    return d[n]
-
 t = int(input())
 for i in range(t):
+    d = [1, 1, 1, 2, 2]
     n = int(input())
-    d = [0] * (n+1)
-    print(dp(n))
+    for i in range(5, n):
+        d.append(d[i-1]+d[i-5])
+    print(d[n-1])
