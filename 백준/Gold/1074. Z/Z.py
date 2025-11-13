5721b41b959f8d14b4841b80c29589edc5ca0f4e
YejunMoon <myj6873@gmail.com>
2025-11-13 22:23:49 +0900

    [Gold V] Title: Z, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n, r, c = map(int, input().split())
+result = 0
+
+while n:
+    n -= 1
+    
+    if r < 2**n and c < 2**n:
+        result += (2**n) * (2**n) * 0
+        
+    elif r < 2**n and c >= 2**n:
+        result += (2**n) * (2**n) * 1
+        c -= (2**n)
+    
+    elif r >= 2**n and c < 2**n:
+        result += (2**n) * (2**n) * 2
+        r -= (2**n)
+    
+    else:
+        result += (2**n) * (2**n) * 3
+        c -= (2**n)
+        r -= (2**n)
+        
+print(result)
