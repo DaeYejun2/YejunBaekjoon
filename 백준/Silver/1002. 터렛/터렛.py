9995eada5e1050d6b05d99301b6fd4ce68c02b40
YejunMoon <myj6873@gmail.com>
2025-11-16 23:59:56 +0900

    [Silver III] Title: 터렛, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    cnt = 0
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    distance = ((x1-x2)**2 + (y1-y2)**2)**0.5
+    if r1 == r2 and distance == 0: print(-1)
+    elif abs(r1-r2) == distance or r1+r2 == distance: print(1)
+    elif abs(r1-r2) < distance < r1+r2: print(2)
+    else: print(0)
