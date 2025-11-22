3a8b91357e9756f8b28210830450bd7214f9a6bc
YejunMoon <myj6873@gmail.com>
2025-11-22 15:01:33 +0900

    [Silver II] Title: 마인크래프트, Time: 708 ms, Memory: 113016 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,36 +1,31 @@
-# 좌표 (i, j)의 가장 위에 있는 블록을 제거하여 인벤토리에 넣는다. -> 2초
-# 인벤토리에서 블록 하나를 꺼내어 좌표 (i, j)의 가장 위에 있는 블록 위에 놓는다. -> 1초
 import sys
 input = sys.stdin.readline
 
-n, m, b = map(int,input().split())
-minecraft = []
+#세로 N, 가로 M, 작업을 시작할 때 인벤토리에는 B개의 블록
+# 좌표 (i, j)의 가장 위에 있는 블록을 제거하여 인벤토리에 넣는다.  -> 2초  del_block
+# 인벤토리에서 블록 하나를 꺼내어 좌표 (i, j)의 가장 위에 있는 블록 위에 놓는다  -> 1초  put_block
+# 땅고르는데 최소 시간. 만약 답이 여러개면? 땅의 높이가 가장 높은 애 출력
 
-for i in range(n):
-    minecraft.append(list(map(int, input().split())))
+n, m, b = map(int, input().split())
+data = [list(map(int,input().split())) for _ in range(n)]
+result = int(1e9)
+lev = 0
 
-ans = int(1e9)
-glevel = 0
-
-for i in range(257): # 땅의 높이 256까지 i 가 1이면 1층인경우 마크땅 전체가 1과 비교
-    use_block = 0
-    take_block = 0
+for i in range(257):    #땅의 높이가 257이니 1층부터 위로
+    #i층. 즉, 0층이 되기 위한 블록의 양과 시간을 구하면 되겠네요
+    del_block, put_block = 0, 0
     for x in range(n):
         for y in range(m):
-            if minecraft[x][y] > i:   # 마크 땅이 i보다 더 높으면 블럭 회수 2초
-                take_block += minecraft[x][y] - i
-            else:              # 마크 땅이 i보다 낮으면 블럭 추가 1초
-                use_block += i - minecraft[x][y]
-# 이해를 위한 2가지 case
-# 전체가 0, 하나만 1, i=0인 경우 생각
-# 전체가 1, 하나만 0, i = 1인 경우 생각
-    if use_block > b + take_block:
+            if data[x][y] >= i:    #블록이 i(층)보다 크거나 같다면, 크니까 블럭을 빼야겠죠?
+                del_block += data[x][y] - i
+            else:
+                put_block += i - data[x][y]
+                
+    if put_block > b + del_block:  # 인벤토리 블럭보다 뺀 블럭이 더 많을 때
         continue
-    
-    cnt = take_block * 2 + use_block
-    
-    if ans >= cnt:
-        ans = cnt
-        glevel = i
+    cnt = del_block*2 + put_block
+    if result >= cnt:
+        result = cnt
+        lev = i
         
-print(ans, glevel)
+print(result, lev)
