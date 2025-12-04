15723deded0246a74e1f4bb3995b4aadd49015ea
YejunMoon <myj6873@gmail.com>
2025-12-04 12:49:07 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int lcm(int a, int b) {
+	if (b == 0) return a;
+	return lcm(b, a % b);
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int ans = lcm(a, b);
+	cout << ans << '\n';
+	cout << (a * b) / ans;
+}
