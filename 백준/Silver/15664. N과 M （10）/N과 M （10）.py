8fef1181d9f92977e072995ead709c4ae63fd310
YejunMoon <myj6873@gmail.com>
2025-12-23 12:58:46 +0900

    [Silver II] Title: N과 M (10), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+data = sorted(list(map(int, input().split())))
+l = []
+
+def backtracking(start):
+    check = 0
+    if len(l) == m:
+        print(' '.join(map(str, l)))
+        return
+    
+    for i in range(start, n):
+        if check != data[i]:
+            check = data[i]
+            l.append(data[i])
+            backtracking(i+1)
+            l.pop()
+    
+backtracking(0)
