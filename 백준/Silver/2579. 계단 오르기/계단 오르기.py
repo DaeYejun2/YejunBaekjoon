902cb2f8f0b1363ceca2ab6b11bc11d6865ba526
YejunMoon <myj6873@gmail.com>
2026-01-17 16:57:05 +0900

    [Silver III] Title: 계단 오르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,17 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n=int(input()) # 계단 개수
-s=[int(input()) for _ in range(n)] # 계단 리스트
-dp=[0]*(n) # dp 리스트
+n = int(input())
+data = [0]*301
 
-if len(s)<=2: # 계단이 2개 이하일땐 그냥 다 더해서 출력
-    print(sum(s))
-else:
-    dp[0] = s[0]
-    dp[1] = s[1] + s[0]  #2까진 값이 바뀌는 경우의 수가 1가지
-    for i in range(2, n):
-        #직전칸에서 올라온 경우, 전전칸에서 올라온 경우 들의 최댓값
-        dp[i] = max(dp[i-3] + s[i-1], dp[i-2]) + s[i]
+for i in range(1, n+1): data[i] = int(input())
+
+dp = [0]*301
+dp[1] = data[1]
+dp[2] = data[1]+data[2]
+dp[3] = max(data[3]+data[1], data[2]+data[3])
+
+for i in range(4, n+1):
+    dp[i] = max(data[i-1]+dp[i-3]+data[i], data[i]+dp[i-2])
     
-    print(dp[-1])
+print(dp[n])
