a0f697652dc85dfc788d29b5e54dedabc7c829b1
YejunMoon <myj6873@gmail.com>
2026-01-25 21:29:18 +0900

    [Silver II] Title: 이항 계수 2, Time: 248 ms, Memory: 54572 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+comb = [[0]*1002 for _ in range(1002)]
+mod = 10007
+
+for i in range(1, 1001):
+    comb[i][0] = comb[i][i] = 1
+    for j in range(1, i):
+        comb[i][j] = (comb[i-1][j] + comb[i-1][j-1]) % mod
+
+print(comb[n][k])
