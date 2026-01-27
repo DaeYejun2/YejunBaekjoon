0cd482962f67c76196231a2b9da272f4f5a2ac1d
YejunMoon <myj6873@gmail.com>
2026-01-27 11:29:54 +0900

    [Silver IV] Title: 30, Time: 56 ms, Memory: 33976 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = input().rstrip()
+
+if '0' not in n: print(-1)
+else:
+    num_sum = 0
+    for i in range(len(n)):
+        num_sum += int(n[i])
+    
+    if num_sum % 3 != 0:
+        print(-1)
+
+    else:
+        sorted_num = sorted(n, reverse=True)
+        print(''.join(map(str, sorted_num)))
