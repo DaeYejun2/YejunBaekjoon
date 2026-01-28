78361e1af5e742b8688ec70ef9a7ecb33c57dfa1
YejunMoon <myj6873@gmail.com>
2026-01-28 16:56:36 +0900

    [Silver V] Title: 날짜 계산, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+e, s, m = map(int, input().split())
+a, b, c = 1, 1, 1
+cnt = 1
+
+while True:
+    if a > 15: a = 1
+    if b > 28: b = 1
+    if c > 19: c = 1
+    if a == e and s == b and m == c: break
+    a += 1
+    b += 1
+    c += 1
+    cnt += 1
+
+print(cnt)
