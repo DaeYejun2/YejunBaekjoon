66d20b28971ef476c68f0420ea631b967f95d7ce
YejunMoon <myj6873@gmail.com>
2026-02-03 18:49:00 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 3584 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def prime(n):
+    if n == 1: return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0: return False
+    return True
+
+def palindrom(n):
+    l = str(n)
+    if l == l[::-1]: return True
+    return False
+
+n = int(input())
+while True:
+    if prime(n) and palindrom(n):
+        print(n)
+        break
+    n += 1
