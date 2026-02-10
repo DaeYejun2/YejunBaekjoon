1c091ae01ce2aca7f8eeae2edf3550300b8f39b4
YejunMoon <myj6873@gmail.com>
2026-02-10 17:43:18 +0900

    [Silver I] Title: 부등호, Time: 112 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -2,40 +2,29 @@ import sys
 input = sys.stdin.readline
 
 k = int(input())
-comm = list(map(str, input().rstrip().split()))
-data = []
-max_value = [-1]
-min_value = [9999999999]
+signs = input().split()
 
-def back(idx):
-    if len(data) == k+1:
-        if max_value[0] < int(''.join(map(str, data))):
-            max_value[0] = int(''.join(map(str, data)))
-        if min_value[0] > int(''.join(map(str, data))):
-            min_value[0] = int(''.join(map(str, data)))
+visited = [False] * 10
+results = []
+
+def check(a, b, op):
+    if op == '<':
+        return a < b
+    return a > b
+
+def solve(depth, current_str):
+    if depth == k + 1:
+        results.append(current_str)
         return
-    
-    if len(data) == 0:
-        for i in range(0, 10):
-            data.append(i)
-            back(0)
-            data.pop()
 
-    if comm[idx] == '<':
-        for j in range(0, 10):
-            if len(data) != 0 and j not in data and j > data[idx]:
-                data.append(j)
-                back(idx+1)
-                data.pop()
-    else:
-        for l in range(0, 10):
-            if len(data) != 0 and l not in data and l < data[idx]:
-                data.append(l)
-                back(idx+1)
-                data.pop()
+    for i in range(10):
+        if not visited[i]:
+            if depth == 0 or check(int(current_str[-1]), i, signs[depth-1]):
+                visited[i] = True
+                solve(depth + 1, current_str + str(i))
+                visited[i] = False
 
-back(0)
-if max_value[0] < 10**(k): print('0'+str(max_value[0]))
-else: print(*max_value)
-if min_value[0] < 10**(k): print('0'+str(min_value[0]))
-else: print(*min_value)
+solve(0, "")
+
+print(results[-1])
+print(results[0])
