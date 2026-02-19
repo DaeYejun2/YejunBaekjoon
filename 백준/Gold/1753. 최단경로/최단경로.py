70f9df8cd3d2da178c8378128dfc5521948eeca7
YejunMoon <myj6873@gmail.com>
2026-02-19 17:15:22 +0900

    [Gold IV] Title: 최단경로, Time: 576 ms, Memory: 69516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,32 +1,31 @@
 import sys
-import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
-v, e = map(int,input().split())
+n, m = map(int,input().split())
 start = int(input())
-graph = [[] for _ in range(v+1)]
-distance = [INF] * (v+1)
+graph = [[] for _ in range(n+1)]
+distance = [INF]*(n+1)
 
-for i in range(e):
+for i in range(m):
     u,v,w = map(int,input().split())
     graph[u].append((v,w))
-    
-def dijkstra(start):
-    q=[]
-    distance[start] = 0
-    heapq.heappush(q, (0,start))
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-                continue
-        for i in graph[now]:  
-            cost = i[1]+dist
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q,(cost, i[0]))
 
-dijkstra(start)
+import heapq
+
+q = []
+distance[start] = 0
+heapq.heappush(q,(0,start))
+
+while q:
+    dist, now = heapq.heappop(q)
+    
+    if distance[now] < dist: continue
+
+    for next_node, cost in graph[now]:
+        if distance[next_node] > dist+cost:
+            distance[next_node] = dist+cost
+            heapq.heappush(q, (dist+cost, next_node))
 
 for i in range(1, len(distance)):
     if distance[i] == INF:
