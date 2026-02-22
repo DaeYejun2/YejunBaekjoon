d7bc432198f84f8516ceb31dfd0c488fcf43b5c7
YejunMoon <myj6873@gmail.com>
2026-02-23 07:30:46 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 400 ms, Memory: 236660 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+str1 = input().rstrip()
+str2 = input().rstrip()
+n = len(str1)
+m = len(str2)
+
+dp = [[0]*(m+1) for _ in range(n+1)]
+max_len = 0
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if str1[i-1]==str2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+            if dp[i][j] > max_len:
+                max_len = dp[i][j]
+        else:
+            dp[i][j] = 0
+
+print(max_len)
