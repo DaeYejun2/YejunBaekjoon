1071a91317dee7041612b103a388422ec39c826c
YejunMoon <myj6873@gmail.com>
2026-02-25 16:13:56 +0900

    [Gold V] Title: 동전, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    coin = list(map(int,input().split()))
+    m = int(input())
+    dp = [0]*(m+1)
+    dp[0] = 1 
+    res = 0
+    for c in coin:
+        for j in range(c, m+1):
+            dp[j] = dp[j] + dp[j-c]
+    print(dp[m])
+    
