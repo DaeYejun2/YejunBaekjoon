ec6875ccf6f7c9678f3b1bde677150e5cd1815e8
YejunMoon <myj6873@gmail.com>
2026-03-10 23:01:19 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 38148 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -3,21 +3,20 @@ from collections import deque
 input = sys.stdin.readline
 inf = 100000
 n, k = map(int, input().split())
-visited = [0] * (inf+1)
 
-def bfs(v):
-    queue = deque()
-    queue.append(v)
-    visited[v] = 0
-    while queue:
-        cur = queue.popleft()
-        for i in (cur - 1, cur + 1, cur*2):
-            if n == k:
-                return visited[cur]
-            if i == k:
-                return visited[cur] + 1
-            if 0 <= i <= inf and not visited[i]:
-                visited[i] = visited[cur] + 1
-                queue.append(i)
-                
-print(bfs(n))
+# x-1, x+1, 2*x 각 위치를 queue에 넣고 갈 수 있는 범위를 확인
+visited = [0] * (inf + 1)
+
+q = deque()
+q.append(n)  # 현재 위치 일단 저장하고
+visited[n] = 1
+
+while q:
+    if visited[k]: break
+    n = q.popleft()
+    for i in [n-1, n+1, 2*n]:
+        if 0 <= i <= inf and not visited[i]:
+            visited[i] = visited[n] + 1
+            q.append(i)
+            
+print(visited[k] - 1)
