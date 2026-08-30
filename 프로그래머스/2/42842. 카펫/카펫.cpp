c7a63392ba10b2421c7e8c34632928e3c3b25c79
YejunMoon <myj6873@gmail.com>
2026-08-30 10:20:14 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,18 +1,17 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
-    int total = brown + yellow;
-    // 따라서 노란색 격자의 가로는 W - 2, 세로는 H - 2가 되며,
-    // (W - 2) * (H - 2) = yellow를 만족해야 합니다.
-    // H의 최솟값은 3부터 시작.
-    for(int i = 3; i < total; i++){
-        if(total % i == 0) {
-            int tmp = total / i;
-            if((tmp-2)*(i-2) == yellow) return {tmp,i};
+    int total = brown+yellow;
+    
+    for(int h = 3; h <= sqrt(total); h++){   // h는 3부터
+        if(total % h == 0){
+            int w = total / h;
+            if((w-2)*(h-2) == yellow) return{w,h};
         }
     }
-
+    return {0,0};
 }
