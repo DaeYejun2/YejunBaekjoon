3f618bad53855a3e7377e02179bfefb1c984ee68
YejunMoon <myj6873@gmail.com>
2026-08-30 23:31:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.86 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,48 +1,52 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <cmath>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
-int bfs(int n, int cuts_idx, vector<vector<int>> wires){
+
+int bfs(int n, int cut_idx, vector<vector<int>> wires){
     vector<vector<int>> graph(n+1);
     for(int i = 0; i < wires.size(); i++){
-        if(cuts_idx == i) continue;
+        if(i==cut_idx) continue;
         
         int u = wires[i][0];
         int v = wires[i][1];
+        
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
     queue<int> q;
-    vector<bool> visited(n+1,false);
-    int cnt = 1;
+    vector<bool> visited(n+1, false);
+    
+    visited[1] = true;
     q.push(1);
-    visited[1]=true;
+    int cnt = 1;
     
     while(!q.empty()){
         int cur = q.front(); q.pop();
+        
         for(int next: graph[cur]){
             if(!visited[next]){
-                cnt++;
                 visited[next] = true;
                 q.push(next);
+                cnt++;
             }
         }
     }
     return cnt;
 }
 
+
+
+
 int solution(int n, vector<vector<int>> wires) {
     int answer = n;
-    
     for(int i = 0; i < wires.size(); i++){
         int cntA = bfs(n, i, wires);
         int cntB = n - cntA;
         
-        int diff = abs(cntA-cntB);
-        answer = min(diff, answer);
+        answer = min(answer, abs(cntA-cntB));
     }
     
     return answer;
