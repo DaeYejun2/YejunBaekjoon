a833dee99d5184f02e8d74e229df43cf615410ce
YejunMoon <myj6873@gmail.com>
2026-09-10 03:12:35 +0900

    [level 1] Title: 실패율, Time: 6.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.cpp
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -7,37 +7,32 @@ using namespace std;
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
     vector<pair<double, int>> fail_rates;
-    sort(stages.begin(), stages.end());
-    // 실패율을 구하고 그 인덱스 값으로. 실패율이 높은 순으로
-    int total_users = stages.size();
-    int idx = 0;
-
+    sort(stages.begin(), stages.end());  // 1, 2, 2, 2, 3, 3, 4, 5
+    
+    int tot_users = stages.size();  // 각 스테이지별 도전자
+    int idx = 0;                    // stage 순회용
+    
     for(int cur_stage = 1; cur_stage <= N; cur_stage++){
-        int cnt = 0;
-        
-        while(idx < stages.size() && stages[idx] == cur_stage){
-            cnt++;
+        int tmp = 0;
+        while(idx < stages.size() && cur_stage == stages[idx]){
+            tmp++;
             idx++;
         }
         
-        // 스테이지에 도달한 유저가 없으면 실패율 0
         double rate = 0.0;
         
-        if(total_users > 0) rate = (double)cnt / total_users;
+        if (tot_users > 0) rate = (double) tmp / tot_users;
         fail_rates.push_back({rate, cur_stage});
-        
-        // 다음 스테이지 도달 인원은 현재 스테이지 실패자만큼 감소
-        total_users -= cnt;
+             
+        tot_users -= tmp;
     }
     
-    sort(fail_rates.begin(), fail_rates.end(),[](const pair<double, int>& a, const pair<double, int>&b){
+    sort(fail_rates.begin(), fail_rates.end(), [](auto& a, auto& b){
         if(a.first == b.first) return a.second < b.second;
         return a.first > b.first;
     });
     
-    for(const auto& p: fail_rates) answer.push_back(p.second);
-    
-    
+    for(auto& a: fail_rates) answer.push_back(a.second);
     
     return answer;
 }
